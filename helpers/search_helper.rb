d5e586ef660f9d5f1d6d6192b92e4aebf78c80f8
Mike Griniezakis <griniezakismike12@gmail.com>
2026-07-16 15:05:10 +0300

    fix crash when pref label is missing in annotator

--- helpers/search_helper.rb
+++ helpers/search_helper.rb
@@ -434,8 +434,7 @@ module Sinatra
           next unless old_class
           doc[:submission] = old_class.submission
           doc[:properties] = MultiJson.load(doc.delete(:propertyRaw)) if include_param_contains?(:properties)
-          instance = LinkedData::Models::Class.read_only(doc)
-          instance.prefLabel = pref_label_by_language(doc)
+          instance = LinkedData::Models::Class.read_only(doc.merge(prefLabel: pref_label_by_language(doc)))
           classes_hash[ont_uri_class_uri] = instance
         end
 
